e50f5af01ccbe68b59a30d2bf4ce0315e4ecce0d
Wahid-Abdul <awahidag@gmail.com>
2022-02-26 18:10:49 +0530

    post call, cors handling

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,18 @@
 
 package com.example.demo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.example.payroll.*;
 
+@CrossOrigin(origins = "*") // Allows requests from all sources
 @SpringBootApplication
 @RestController
 public class DemoApplication {
@@ -24,9 +26,26 @@ public class DemoApplication {
         return String.format("Hello %s!", name);
     }
 
-    @PostMapping("/employees")
-    public String newEmployee(@RequestBody Employee newEmployee) {
-
-           return newEmployee.getName();
+    @GetMapping("/hello2")
+    @ResponseBody
+    public Employee hello2() {
+        Employee emp = new Employee("anme", "adasres");
+        return emp;
     }
+
+    @PostMapping("/employees")
+    @ResponseBody
+    public Employee newEmployee(@RequestBody Employee newEmployee) {
+//        curl -X POST -H "Content-Type: application/json" \
+//        -d '{"name": "linuxizasdasdasdasde", "address": "linuxize@example.com"}' \
+//        http://localhost:8080/employees
+
+//        The above curl request works as expected but postman isn't budging
+
+        return newEmployee;
+    }
+
+
+
+
 }
